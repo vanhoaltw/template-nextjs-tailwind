e87ee861c23388caab137254e69d2cb29f35891d
moondevvn@gmail.com <moondevvn@gmail.com>
2022-01-19 02:01:56 +0700

    section-1 change

---
+++ components/layout/Mobile/Section1.tsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Section1() {
+  return (
+    <div className="h-screen relative">
+      {/* Khung */}
+      <div className="bg">
+        <div className=" top-10 z-40 flex justify-between relative px-4 ">
+          <button className=" z-0 w-4/12  px-4">
+            <img src="/images/section-1/btn_home.png" alt="bg-image" />
+          </button>
+          <button className=" z-0 w-4/12  px-4">
+            <img src="/images/section-1/btn_pitch.png" alt="bg-full" />
+          </button>
+        </div>
+        <img
+          src="/images/section-1/khung-1.png"
+          alt="cover"
+          className="absolute top-0 z-20 mt-1"
+        />
+        <img
+          src="/images/section-1/a-1.png"
+          alt="asset-1"
+          className="absolute top-0 z-30 mt-1 p-32 pt-0"
+        />
+        <img
+          src="/images/section-1/logo.png"
+          alt="logo"
+          className="absolute top-24 left-32 z-20 mt-1 w-1/3 "
+        />
+        <img
+          src="/images/section-1/bg-1.png"
+          alt="bg-image"
+          className="absolute top-0 z-10 "
+        />
+        <img
+          src="/images/section-1/bg.png"
+          alt="bg-full"
+          className="absolute top-96 z-0 mt-20 "
+        />
+        {/* char */}
+        <img
+          src="/images/section-1/char-1.png"
+          alt="bg-image"
+          className="absolute top-80 z-40 w-2/6"
+        />
+        <img
+          src="/images/section-1/char-2.png"
+          alt="bg-full"
+          className="absolute top-96 left-64 z-40 w-1/4  "
+        />
+        <button className="absolute bottom-24 left-auto left-28 z-40 w-5/12 mr-2 ">
+          <img src="/images/section-1/btn_play.png" alt="bg-full" />
+        </button>
+      </div>
+      {/* char */}
+    </div>
+  )
+}
+
+Section1.propTypes = {}
+
+export default Section1
--- components/layout/MobileLayout.tsx
+++ components/layout/MobileLayout.tsx
@@ -1,58 +1,11 @@
 import React from 'react'
 import Image from 'next/image'
+import Section1 from './Mobile/Section1'
 function MobileLayout() {
   return (
     <div className="block">
       {/* section-1 */}
-      <div className="bg-landing-background bg-contain bg-cover h-screen">
-        <div className="container h-5/6 p-8">
-          {/* Khung */}
-          <div className="bg-border-section-1 bg-cover  h-full  relative ">
-            <div className="absolute top-7 left-11 flex justify-between w-64">
-              <button className="bg-transparent">
-                <Image
-                  src="/images/buttons/home.png"
-                  alt="Tappy Logo"
-                  width={70}
-                  height={30}
-                />
-              </button>
-              <button className="bg-transparent">
-                <Image
-                  src="/images/buttons/pitch.png"
-                  alt="Tappy Logo"
-                  width={70}
-                  height={30}
-                />
-              </button>
-            </div>
-            <div className="absolute top-20 left-28 ">
-              <Image
-                src="/images/logo.png"
-                alt="Tappy Logo"
-                width={125}
-                height={125}
-              />
-            </div>
-            <div className="absolute top-40 left-0 ">
-              <Image
-                src="/images/dautay.png"
-                alt="Picture of the author"
-                width={100}
-                height={150}
-              />
-            </div>
-            <div className="absolute bottom-40 right-0 ">
-              <Image
-                src="/images/cucai.png"
-                alt="Picture of the author"
-                width={100}
-                height={150}
-              />
-            </div>
-          </div>
-        </div>
-      </div>
+      <Section1 />
     </div>
   )
 }
