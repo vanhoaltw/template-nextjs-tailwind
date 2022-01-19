8a4aec695d4c5baab3a74e29d479d1bc6508fb23
moondevvn@gmail.com <moondevvn@gmail.com>
2022-01-19 22:45:18 +0700

    fix overlap section

--- components/layout/MobileLayout.tsx
+++ components/layout/MobileLayout.tsx
@@ -4,11 +4,16 @@ import Section2 from './Mobile/section-2'
 import Section1 from './Mobile/Section1'
 function MobileLayout() {
   return (
-    <div className="block">
-      {/* section-1 */}
-      <Section1 />
-      <Section2 />
-    </div>
+    <>
+      {' '}
+      <div className="block">
+        {/* section-1 */}
+        <Section1 />
+      </div>
+      <div className="block mt-72">
+        <Section2 />
+      </div>
+    </>
   )
 }
 export default MobileLayout
