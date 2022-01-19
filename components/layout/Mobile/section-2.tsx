f88324c7722403efb7fbf153ac10dee6cbf20dcf
Vinh Ngo <nanyangbk@gmail.com>
2022-01-20 00:42:54 +0700

    fix: UI scale error

--- components/layout/Mobile/section-2.tsx
+++ components/layout/Mobile/section-2.tsx
@@ -17,49 +17,61 @@ function Section2() {
           <img
             src="/images/section-2/bg1.jpg"
             alt=""
-            className="absolute top-0 z-0 mt-12"
+            className="absolute top-0 z-0 mt-12 sm:mt-20"
           />
         </div>
-        <div className="icons covid absolute top-4 right-0 z-20">
-          <img
-            src="/images/section-2/covid1.PNG"
-            alt=""
-            className="z-20 w-20"
-          />
-          <img
-            src="/images/section-2/stone1.png"
-            alt=""
-            className="z-20 w-24"
-          />
+        <div className="icons covid absolute top-4 right-0 z-20 w-full flex items-center justify-end">
+          <div className="w-full flex flex-col items-end justify-end">
+            <img
+              src="/images/section-2/covid1.PNG"
+              alt=""
+              className="z-20 w-3/12 mr-1"
+            />
+            <img
+              src="/images/section-2/stone1.png"
+              alt=""
+              className="z-20 w-3/12"
+            />
+          </div>
         </div>
-        <div className="">
-          <img src="/images/section-2/tab1.JPG" alt="" className="z-50" />
+      </div>
+      <div className="relative">
+        <img src="/images/section-2/tab1.JPG" alt="" className="z-50" />
+        <div className="w-full absolute top-0 flex flex-col">
           <img
             src="/images/section-2/egg.PNG"
             alt=""
-            className="z-100 absolute top-80 mt-12"
+            className="z-100 absolute -top-14 sm:-top-20 w-full"
           />
-        </div>
-        <div className="flex items-center justify-around absolute top-96 mt-24 w-full">
-          <img src="/images/section-2/bt1.PNG" alt="" className="z-100 w-8" />
-          <img
-            src="/images/section-2/card1.png"
-            alt=""
-            className="z-100 w-28"
-          />
-          <img
-            src="/images/section-2/card2.PNG"
-            alt=""
-            className="z-100 w-28"
-          />
-          <img src="/images/section-2/bt2.PNG" alt="" className="z-100 w-8" />
-        </div>
-        <div className="flex items-center justify-center w-full absolute top-96 mt-64">
-          <img
-            src="/images/section-2/heart.PNG"
-            alt=""
-            className="z-100 w-14"
-          />
+          <div className="flex items-center justify-around mt-14 sm:mt-24">
+            <img
+              src="/images/section-2/bt1.PNG"
+              alt=""
+              className="z-100 w-1/12"
+            />
+            <img
+              src="/images/section-2/card1.png"
+              alt=""
+              className="z-100 w-1/3 sm:w-1/4"
+            />
+            <img
+              src="/images/section-2/card2.PNG"
+              alt=""
+              className="z-100 w-1/3 sm:w-1/4"
+            />
+            <img
+              src="/images/section-2/bt2.PNG"
+              alt=""
+              className="z-100 w-1/12"
+            />
+          </div>
+          <div className="flex items-center justify-center w-full mt-1 sm:mt-16">
+            <img
+              src="/images/section-2/heart.PNG"
+              alt=""
+              className="z-100 w-1/12"
+            />
+          </div>
         </div>
       </div>
       <div className="relative">
