e8dd7f9e6d53bc81efc8c92a6a9ab8c3632820df
moondevvn@gmail.com <moondevvn@gmail.com>
2022-01-22 02:45:40 +0700

    fix scale all mobile ignore galaxy duo

--- components/layout/Mobile/Section3.tsx
+++ components/layout/Mobile/Section3.tsx
@@ -6,62 +6,68 @@ function Section3() {
 
   return (
     <>
-      <div className="relative h-screen">
+      <div className="relative">
         <img
           src="/images/section-3/infomation_1.png"
           alt=""
           className="absolute -top-14 left-1 z-10 p-2"
         />
+        <div className="absolute bottom-[-30%]  flex items-center justify-center w-full  z-20">
+          <button className="z-30 w-1/2 ">
+            <img src="/images/section-3/spin_button.PNG" alt="bg-full" />
+          </button>
+        </div>
+
         <div className="bg">
           <img
             src="/images/section-3/background_1.JPG"
             alt=""
-            className="absolute top-0 z-0"
+            className="static top-0 z-0"
           />
         </div>
-        <div className=" relative h-72  md:h-96">
-          <div className="absolute bottom-0  flex items-center justify-center w-full  z-20">
-            <button className="z-30 w-1/2 ">
-              <img src="/images/section-3/spin_button.PNG" alt="bg-full" />
-            </button>
-          </div>
-          <div className=" h-1/2 relative top-80 flex  items-center justify-center w-full  z-20">
-            <img
-              src="/images/section-3/spin_egg.PNG"
-              alt="bg-full"
-              className="z-30 w-5/6  absolute top-2 "
-            />
-            <div className=" absolute top-28 flex items-center justify-center w-full  z-50">
-              <button className="z-30 w-1/4  ">
-                <img src="/images/section-3/spin_button2.PNG" alt="bg-full" />
-              </button>
-            </div>
-          </div>
+      </div>
+      <div className="relative">
+        <div className=" absolute top-[20%] flex  items-center justify-center w-full  z-20">
+          <img
+            src="/images/section-3/spin_egg.PNG"
+            alt="bg-full"
+            className="z-30 w-5/6  absolute top-2 "
+          />
+          <button className="z-30 w-1/4 mt-[30%] ">
+            <img src="/images/section-3/spin_button2.PNG" alt="bg-full" />
+          </button>
         </div>
-        <div className=" absolute bottom-8 flex items-center justify-center w-full  z-20">
+        <div className="bg">
+          <img
+            src="/images/section-3/background_1_1.JPG"
+            alt=""
+            className="static top-0 z-0"
+          />
+        </div>
+      </div>
+      <div className="relative">
+        <div className=" absolute top-[-20%] flex items-center justify-center w-full  z-20">
           <img
             src="/images/section-3/egg_1.PNG"
             alt="bg-full"
             className="z-30 w-9/12"
           />
         </div>
-      </div>
-      <div className="relative h-[2070px]">
-        <div className="absolute -top-10 flex items-center justify-center w-full px-2">
+        <div className="absolute top-[3%] flex items-center justify-center w-full px-2">
           <img
             src="/images/section-3/iron_chain.png"
             alt=""
             className="z-100 w-11/12 "
           />
         </div>
-        <div className="absolute -top-10 flex items-center justify-center w-full px-2">
+        <div className="absolute top-[3%] flex items-center justify-center w-full px-2">
           <img
             src="/images/section-3/open_button.PNG"
             alt=""
             className="z-100 w-2/12 "
           />
         </div>
-        <div className="absolute top-12 flex items-center justify-center w-full px-4">
+        <div className="absolute top-[22%] flex items-center justify-center w-full px-8">
           <img
             src="/images/section-3/random_card.PNG"
             alt=""
@@ -73,31 +79,54 @@ function Section3() {
             className="z-100 w-1/2 px-2"
           />
         </div>
-        <div className="absolute bottom-[56%] flex items-center justify-center w-full px-2">
+        <div className="bg">
           <img
-            src="/images/section-3/window_scene.JPG"
+            src="/images/section-3/background_2_1.PNG"
             alt=""
-            className="z-0  "
+            className="static top-0 z-0"
           />
         </div>
-        <div className="absolute bottom-[52%] left-2 flex items-center justify-center w-full ">
+      </div>
+      <div className="relative">
+        <div className="bg">
+          <img
+            src="/images/section-3/background_2_2.PNG"
+            alt=""
+            className="static top-0 z-10"
+          />
+          <img
+            src="/images/section-3/background_2_2.PNG"
+            alt=""
+            className="absolute top-0 z-10"
+          />
+          <img
+            src="/images/section-3/door_scene.JPG"
+            alt=""
+            className="absolute top-[46%]  z-0 w-full max-w-[400px]"
+          />
+        </div>
+      </div>
+      <div className="relative">
+        <div className="absolute top-[-23%] left-2 flex items-center justify-center w-full ">
           <img
             src="/images/section-3/character_potato.PNG"
             alt=""
             className="z-100 w-1/4"
           />
         </div>
-        <div className="absolute bottom-1/4 left-2 flex items-center justify-center w-full p-8">
+        <div className="absolute bottom-[10%] left-2 flex items-center justify-center w-full p-8">
           <img src="/images/section-3/stone_1.PNG" alt="" className="z-100 " />
         </div>
-        <div className="absolute bottom-[9%] flex items-center justify-center w-full px-2">
+        <div className="bg">
           <img
-            src="/images/section-3/door_scene.JPG"
+            src="/images/section-3/background_2_3.PNG"
             alt=""
-            className="z-0  "
+            className="static top-0 z-10"
           />
         </div>
-        <div className="absolute bottom-[9%] flex items-start  w-3/4 px-4 ml-6">
+      </div>
+      <div className="relative">
+        <div className="absolute bottom-[30%] flex items-start  w-3/4 px-4 ml-10">
           <img
             src="/images/section-3/character_pineapple.PNG"
             alt=""
@@ -106,9 +135,19 @@ function Section3() {
         </div>
         <div className="bg">
           <img
-            src="/images/section-3/background_2.PNG"
+            src="/images/section-3/background_2_4.PNG"
             alt=""
-            className="absolute -top-10 z-0"
+            className="static top-0 z-10"
+          />
+          <img
+            src="/images/section-3/background_2_4.PNG"
+            alt=""
+            className="absolute top-0 z-10"
+          />
+          <img
+            src="/images/section-3/door_scene.JPG"
+            alt=""
+            className="absolute top-[10%]  z-0 w-full"
           />
         </div>
       </div>
