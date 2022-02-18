ca2851fdf7f51a82afd1f5d5610f8f9c55c7cbcc
HoaNguyen <vanhoaltw@gmail.com>
2022-02-18 16:57:00 +0700

    first-commit

--- components/layout/Desktop/index.tsx
+++ components/layout/Desktop/index.tsx
@@ -1,19 +1,6 @@
 import React from 'react'
-import Image from 'next/image'
-import DesktopSection1 from './section-1'
-import DesktopSection2 from './section-2'
+
 function DesktopLayout() {
-  return (
-    <>
-      <div className="block">
-        <DesktopSection1 />
-      </div>
-      <div className="block">
-        {/* <Section2 /> */}
-        <DesktopSection2 />
-      </div>
-      <div className="block">{/* <Section3 /> */}</div>
-    </>
-  )
+  return <>Desktop</>
 }
 export default DesktopLayout
--- components/layout/Mobile/MobileLayout.tsx
+++ components/layout/Mobile/MobileLayout.tsx
@@ -1,21 +1,6 @@
 import React from 'react'
-import Image from 'next/image'
-import Section2 from './section-2'
-import Section1 from './Section1'
-import Section3 from './Section3'
+
 function MobileLayout() {
-  return (
-    <>
-      <div className="block w-screen overflow-hidden">
-        <Section1 />
-      </div>
-      <div className="block w-screen overflow-hidden">
-        <Section2 />
-      </div>
-      <div className="block w-screen">
-        <Section3 />
-      </div>
-    </>
-  )
+  return <>Mobile</>
 }
 export default MobileLayout
